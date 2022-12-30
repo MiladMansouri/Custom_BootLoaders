2ea1a1c5594aae71246426445c6e7f4fddf68384
Milad Mansouri <Miladmh72@yahoo.com>
2022-12-31 00:10:03 +0330

    add get Mcu CID, Flash Protection Level and Flash Size

--- STM32_Custom_BootLoader_UART/BootLoader/Core/Inc/bootLoader.h
+++ STM32_Custom_BootLoader_UART/BootLoader/Core/Inc/bootLoader.h
@@ -3,25 +3,36 @@
 #include "stdint.h"
 
 #define BUFFER_SIZE 255
-#define BL_VERSION 0x41 
+#define BL_VERSION 0x10
 
 typedef enum
 {
-  BL_GET_VERSION = 0x20U,
-  BL_JUMP_TO_APP = 0x21U,
-}Te_UID;
+  CMD_GET_VERSION = 0x20U,
+  CMD_JUMP_TO_APP = 0x21U,
+  CMD_GET_MCU_CID = 0x22U,
+  CMD_GET_RDP_STATUS = 0x23U,
+  CMD_GET_FLAHS_SIZE = 0x24U,
+}Te_BL_UID;
 
 typedef enum
 {
-  ANS_GET_VERSION = 0x1U,
-  ANS_JUMP_TO_APP = 0x0U,
-  
-}Te_ANSWER_SIZE;
+  ANS_GET_VERSION = 0x01U,
+  ANS_JUMP_TO_APP = 0x00U,
+  ANS_GET_MCU_CID = 0x02U,
+  ANS_GET_RDP_STATUS = 0x01U,
+  ANS_GET_FLAHS_SIZE = 0x02U,
+}Te_BL_ANSWER_SIZE;
 
 void bootLoaderProcessing(void);
 
 static void bootLoaderGetVersionProcessing(void);
 static void bootLoaderJumpToApplication(void);
-static void sendPositiveAnswer(uint8_t size);
+static void getMcuCidNumberProcessing(void);
+static void getMcuFlashProtectionLevel(void);
+static void getMcuFlashSizeinKb(void);
 
+
+static void sendPositiveAnswer(uint8_t size);
+static void sendNegetiveAnswer(void);
+static void readyForNewCommand(void);
 #endif
--- STM32_Custom_BootLoader_UART/BootLoader/Core/Src/bootLoader.c
+++ STM32_Custom_BootLoader_UART/BootLoader/Core/Src/bootLoader.c
@@ -5,100 +5,114 @@
 #include "iwdg.h"
 #include <string.h>
 
-#define bufferSize 255
-uint8_t blBuffer[bufferSize] = {0};
-uint8_t receiveComplete = 0u;
-uint8_t reciveSize  = 0u;
-uint8_t cmd = 0x00u;
+uint8_t Rx_data[2] = {0}, receiveBuffer[BUFFER_SIZE] = {0};
+uint8_t receiveComplete = 0, Rx_indx = 0;
 
-uint8_t ansBuff[BUFFER_SIZE];
+uint8_t ansBuffer[BUFFER_SIZE];
 
 //XModem
+/**
+  * @brief  UART Interrupt Call Back
+  * @note   None
+  * @param  None
+  * @retval None
+  */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
     if(huart->Instance == USART2)
     {
-        receiveComplete = 1;   
+        if (Rx_indx == 0) 
+        {
+            for (uint8_t i = 0; i < BUFFER_SIZE; i++) 
+                receiveBuffer[i] = 0; 
+        }else{} 
+        if(Rx_data[0] != '\n') //if received data different from ascii 13 (enter)
+        {
+            receiveBuffer[Rx_indx++] = Rx_data[0];     //add data to receiveBuffer
+        }
+        else            //if received data = 13
+        {
+            Rx_indx = 0;
+            receiveComplete = 1; //received complete, data is ready to read
+            HAL_UART_AbortReceive_IT(&huart2);
+        }
+        HAL_UART_Receive_IT(&huart2, Rx_data, 1);
     }
     else
     {}
 }
-
+/**
+  * @brief  Process Boot Loader Commends
+  * @note   None
+  * @param  None
+  * @retval None
+  */
 void bootLoaderProcessing(void)
 {
-<<<<<<< HEAD
     HAL_UART_Transmit(&huart2,"Start bootLoader Processing!\r\n",30,1000);
-    HAL_UART_Receive_IT(&huart2, blBuffer, 2);
+    HAL_UART_Receive_IT(&huart2, receiveBuffer, 1);
     while(1)
     {
         if(receiveComplete == 1)
         {
-            receiveComplete = 0;
-            cmd = blBuffer[0];
-            reciveSize = blBuffer[1];
+            switch (receiveBuffer[0])// switch on UID
+            {
+                case CMD_GET_VERSION:
+                {
+                    bootLoaderGetVersionProcessing();
+                }break;
+                case CMD_JUMP_TO_APP:
+                {
+                    bootLoaderJumpToApplication();
+                }break;
+                case CMD_GET_MCU_CID:
+                {
+                    getMcuCidNumberProcessing();
+                }break;
+                case CMD_GET_RDP_STATUS:
+                {
+                    getMcuFlashProtectionLevel();
+                }break;
+                case CMD_GET_FLAHS_SIZE:
+                {
+                    getMcuFlashSizeinKb();
+                }break;
+                
+                default:
+                {
+                    sendNegetiveAnswer();
+                }break;
+                
+            }
         }
         else
         {
             
         }
-        switch (blBuffer[0])
-        {
-            case BL_GET_VERSION:
-            {
-                bootLoaderGetVersionProcessing();
-            }break;
-            case BL_JUMP_TO_APP:
-            {
-                bootLoaderJumpToApplication();
-            }break;
-            default:
-            {
-                memset(blBuffer,0,BUFFER_SIZE);
-                HAL_UART_Receive_IT(&huart2, blBuffer, 2);
-                receiveComplete = 0;
-            }break;
-            
-        }
-        
         HAL_IWDG_Refresh(&hiwdg);
     }
-=======
-//  HAL_UART_Transmit(&huart2,"Start bootLoader Processing!\r\n",30,1000);
-//  while(1)
-//  {
-//    reciveSize = 0;
-//    HAL_UART_Receive(&huart2,&blBuffer[0],1,HAL_MAX_DELAY);
-//    reciveSize = blBuffer[0] - '0';
-//    HAL_UART_Receive(&huart2,&blBuffer[1],reciveSize,HAL_MAX_DELAY);
-//    switch (blBuffer[1])
-//    {
-//      case BL_GET_VERSION:
-//      {
-//        bootLoaderGetVersionProcessing();
-//      }break;
-//      default:
-//      {
-//        memset(blBuffer,0,BUFFER_SIZE);
-//      }break;
-//      
-//    }
-//  }
->>>>>>> 3831ada184fb707c389b21c64c547f811064e1c7
 }
-/*
-return BootLoader Version
-*/
+/**
+  * @brief  return boot loader version to user
+  * @note   None
+  * @param  None
+  * @retval None
+  */
 static void bootLoaderGetVersionProcessing(void)
 {
     //  Send Ack + answer size 
-    ansBuff[0] = BL_VERSION;
+    ansBuffer[0] = BL_VERSION;
     sendPositiveAnswer(ANS_GET_VERSION);
 }
-
+/**
+  * @brief  clear RAM data and software reset 
+  * @note   set 0x20004FFF - 0x20004FFC ram address to 0xFF
+  * @param  None
+  * @retval None
+  */
 static void bootLoaderJumpToApplication(void)
 {
     sendPositiveAnswer(ANS_JUMP_TO_APP);
-    
     *(volatile uint8_t*)0x20004FFF = 0xFFU;
     *(volatile uint8_t*)0x20004FFE = 0xFFU;
     *(volatile uint8_t*)0x20004FFD = 0xFFU;
@@ -106,17 +120,85 @@ static void bootLoaderJumpToApplication(void)
     HAL_NVIC_SystemReset();
 }
 
+/**
+  * @brief  Get MCU CID number and send it 
+  * @note   None
+  * @param  None
+  * @retval None
+  */
+static void getMcuCidNumberProcessing(void)
+{
+    uint16_t cid;
+	cid = (uint16_t)(DBGMCU->IDCODE) & 0x0FFF;
+    ansBuffer[0] = cid >> 8;
+    ansBuffer[1] = cid;
+    sendPositiveAnswer(ANS_GET_MCU_CID);
+}
+/**
+  * @brief  Return MCU Flash Protection Level
+  * @note   None
+  * @param  None
+  * @retval None
+  */
+static void getMcuFlashProtectionLevel(void)
+{
+    uint8_t rdp_status=0;
+	FLASH_OBProgramInitTypeDef  ob_handle;
+	HAL_FLASHEx_OBGetConfig(&ob_handle);
+	rdp_status = (uint8_t)ob_handle.RDPLevel;
+    ansBuffer[0] = rdp_status;
+    sendPositiveAnswer(ANS_GET_RDP_STATUS);
+}
+
+static void getMcuFlashSizeinKb(void)
+{
+    
+    uint16_t flashSizeKb = *(const uint16_t*)FLASHSIZE_BASE;
+    ansBuffer[0] = flashSizeKb >> 8;
+    ansBuffer[1] = flashSizeKb;
+    sendPositiveAnswer(ANS_GET_FLAHS_SIZE);
+}
+/*****************************************************************************/
+/**
+  * @brief  Send Positive Answer
+  * @note   use ansBuffer to add answer to positive answer
+  * @param  uint8_t size (use Te_ANSWER_SIZE)
+  * @retval None
+  */
 static void sendPositiveAnswer(uint8_t size)
 {
-    blBuffer[0] = size;
-    blBuffer[1] += 0x40;
-    HAL_UART_Transmit(&huart2,blBuffer,2,500);
+    receiveBuffer[0] += 0x40;
     for(uint8_t index = 0; index <= size; index++)
     {
-        blBuffer[index] = ansBuff[index];
+        receiveBuffer[index + 1] = ansBuffer[index];
     }
-    HAL_UART_Transmit(&huart2,blBuffer,size,500);
-    memset(blBuffer,0,BUFFER_SIZE);
-    
-    HAL_UART_Receive_IT(&huart2, blBuffer, 2);
+    HAL_UART_Transmit(&huart2,receiveBuffer,size + 1,500);
+    HAL_UART_Transmit(&huart2,"\n",1,500);
+    readyForNewCommand();
+}
+
+/**
+  * @brief  send Negetive Answert
+  * @note   None
+  * @param  None
+  * @retval None
+  */
+static void sendNegetiveAnswer(void)
+{
+    receiveBuffer[0] = 0x7F;
+    HAL_UART_Transmit(&huart2,receiveBuffer,1,500);
+    HAL_UART_Transmit(&huart2,"\n",1,500);
+    readyForNewCommand();
+}
+/**
+  * @brief  Clear the buffers and enable interrupt to recevie again
+  * @note   None
+  * @param  None
+  * @retval None
+  */
+static void readyForNewCommand(void)
+{
+    memset(receiveBuffer,0,BUFFER_SIZE);
+    receiveComplete = 0;
+    HAL_UART_Receive_IT(&huart2, Rx_data, 1);
 }
